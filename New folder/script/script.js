71b8890f9b4f52977091839bf113208e726e66f8
webkage <middlemicah@gmail.com>
2021-01-16 13:49:52 -0500

    add responsive element to slider
    Does not fix issue with missing slide and images shrinking.

--- New folder/script/script.js
+++ New folder/script/script.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
     var autoplaySlider = $('#autoplay').lightSlider({
         auto:false,
+        item:4,
+        slideMove:2,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
         loop:true,
         pauseOnHover: true,
+        responsive : [
+    {
+        breakpoint:800,
+        settings: {
+            item:3,
+            slideMove:1,
+            slideMargin:6,
+          }
+    },
+    {
+        breakpoint:480,
+        settings: {
+            item:2,
+            slideMove:1
+          }
+    }
+],
         onBeforeSlide: function (el) {
             $('#current').text(el.getCurrentSlideCount());
         }
     });
     $('#total').text(autoplaySlider.getTotalSlideCount());
+
 });
